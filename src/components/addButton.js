bc5e5a626a2a199964b835edd06703ca1860fc8b
Enhmunh-E <enhuush0704@gmail.com>
2020-12-21 17:40:19 +0800

    added delete and status

--- src/components/addButton.js
+++ src/components/addButton.js
@@ -1,32 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { firebase, db, storage } from '../firebase'
 const AddButton = () => {
+    const [upl, setUpl] = useState(false);
+    const [perc, setPerc] = useState(0);
     const uploadImageToStorage = (file) => {
+        setUpl(true);
         var storageRef = storage.ref();
-        storageRef.child(`img-storage/${file.name}`).put(file);
-        // uploadTask.on('state_changed', function(snapshot){
-        //     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        //     console.log('Upload is ' + progress + '% done');
-        //     switch (snapshot.state) {
-        //     case firebase.storage.TaskState.PAUSED:
-        //         console.log('Upload is paused');
-        //         break;
-        //     case firebase.storage.TaskState.RUNNING:
-        //         console.log('Upload is running');
-        //         break;
-        //     }
-        // }, function(error) {
-        // }, function() {
-            window.location.reload();
-        // });
+        var uploadTask = storageRef.child(`img-storage/${file.name}`).put(file);
+        uploadTask.on('state_changed', (snapshot) => {
+            var progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+            setPerc(progress);
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED:
+                console.log('Upload is paused');
+                break;
+            case firebase.storage.TaskState.RUNNING:
+                console.log('Upload is running');
+                break;
+            }
+        }, function(error) {
+        }, function() {
+            console.log('upload done');
+            setTimeout(() => {
+                setUpl(false);   
+                window.location.reload();
+            }, 1000);
+        });
     }
     return (
+        <>
         <div style={{ backgroundColor: 'rgb(77,121,213)', width: '60%', borderRadius: '30px', height: '50px', display: 'flex', justifyContent: 'center' }}>
             <div style={{ fontSize: '24px', color: 'white', textAlign: 'center', display: 'flex', alignItems: 'center' }}>
                 Upload File
             </div>
             <input type='file' className='file_upload' accept='image/*' onChange={(e) => uploadImageToStorage(e.target.files[0])}/>
         </div>
+        <div style={{border: '1px solid black', width: '80%', borderRadius: '5px', height: '25px', display:`${upl ? 'block': 'none'}`,  margin: '10px 0px'}}>
+            <div style={{borderRadius: '5px', width:`${perc}%`, height: '25px', backgroundColor: 'cyan', textAlign: 'center'}}>
+                {perc+'%'}
+            </div>
+        </div>
+        </>
     )
 }
 
--- src/components/itemList.js
+++ src/components/itemList.js
@@ -1,16 +1,31 @@
 import React from 'react'
-
+import { firebase, db, storage } from '../firebase'
 import fileIcon from './../file.png'
 
 const ItemList = ({ name = 'name', date = '2020/20/20', url='' }) => {
+    const del = () => {
+        var storageRef = storage.ref();
+        var deleteRef = storageRef.child(`img-storage/${name}`);
+        deleteRef.delete().then(function() {
+            console.log('File Deleted Successfully!!!');
+        }).catch(function(error) {
+            console.log('Uh-oh, an error occurred!')
+        });
+        window.location.reload();
+    } 
     return (
-        <div onClick={() => window.location.href = url} style={{ display: 'flex', flexDirection: 'row', height: '50px', padding: '10px', margin: '30px', backgroundColor: 'white', borderRadius: '5px' }}>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <img src={fileIcon} height={'30px'} width={'30px'} />
+        <div style={{ display: 'flex', flexDirection: 'row', height: '50px', padding: '10px', margin: '30px', backgroundColor: 'white', borderRadius: '5px', justifyContent: 'space-between' }}>
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                <div onClick={() => window.location.href = url} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <img src={fileIcon} height={'30px'} width={'30px'} />
+                </div>
+                <div onClick={() => window.location.href = url} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', marginLeft: '10px' }}>
+                    <div style={{ color: 'rgba(0,0,0,.72)', fontSize: '16px' }}> {name} </div>
+                    <div style={{ color: 'rgba(0,0,0,.54)', fontSize: '14px' }}> {date} </div>
+                </div>
             </div>
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', marginLeft: '10px' }}>
-                <div style={{ color: 'rgba(0,0,0,.72)', fontSize: '16px' }}> {name} </div>
-                <div style={{ color: 'rgba(0,0,0,.54)', fontSize: '14px' }}> {date} </div>
+            <div onClick={del} style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                X
             </div>
         </div>
     )
